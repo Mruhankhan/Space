b3f7318f9f1ae400f2e090f36d95f06378f2afdc
Muhammad Khan <s1942405@online.houstonisd.org>
2026-06-27 16:52:34 -0500

    WORK

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    fs: {
+      // Allow Vite to serve files from the workspace root as well, but prefer local MICS/src
+      allow: [path.resolve(__dirname, '..'), path.resolve(__dirname)]
+    }
+  },
+  resolve: {
+    alias: {
+      // Resolve '/src' to the MICS local src so imports use MICS/src by default
+      '/src': path.resolve(__dirname, 'src')
+    }
+  }
 })
